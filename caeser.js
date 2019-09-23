8c754915f9e61b93f70cf0d003276b92d6420cf4
Tyler <t.mar92@gmail.com>
2019-09-23 18:55:52 +0000

    initial commit

---
+++ caeser.js
@@ -0,0 +1,34 @@
+const encrypt = function(plaintext, key) {
+  let outgoing = "";
+  const alphabet = [
+    "a", "b", "c", "d", "e",
+    "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "o",
+    "p", "q", "r", "s", "t",
+    "u", "v", "w", "x", "y",
+    "z"
+  ];
+  let regex = /[a-z]/;
+  plaintext = plaintext.toLowerCase();
+
+  for (let i = 0; i < plaintext.length; i++) {
+    let index = alphabet.indexOf(plaintext[i]);
+    // console.log("index before if: ", index);
+    if (regex.test(plaintext[i])) {
+      index += key;
+      (index < 0)
+        ? outgoing += alphabet[alphabet.length + index]
+        : (index >= alphabet.length)
+          ? outgoing += alphabet[index % alphabet.length]
+          : outgoing += alphabet[index];
+    } else {
+      outgoing += plaintext[i];
+    }
+  }
+  return outgoing;
+  // console.log(regex.test(plaintext[0]));
+};
+
+module.exports = {encrypt};
+
+console.log(encrypt("animal", -2));
